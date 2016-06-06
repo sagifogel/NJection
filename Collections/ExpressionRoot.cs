b10d53afe2e9e0c0246eb7292dfd0646b1141b09
Sagi <sagifogel@hotmail.com>
2016-06-06 23:26:13 +0300

    reset

--- Collections/ExpressionRoot.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Linq.Expressions;
-namespace NJection.Collections
-{
-    public class ExpressionRoot
-    {
-        public ITreeNode<Expression> First { get; internal set; }
-    }
-}
